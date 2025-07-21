ea7a1b7f2c7e56737e55bfe6e6334217929e15e0
Jerry Febriano <febrianojerry@gmail.com>
2025-07-21 13:39:01 +0700

    feat: init

---
+++ JARVIS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JARVIS
+//
+//  Created by Jerry Febriano on 21/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
